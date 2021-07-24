c62ee78694740badc92042866c6786c82d517e81
erick <ericklarsenn@gmail.com>
2021-07-25 00:18:49 +0700

    set login function

--- src/components/organisms/LoginScreen/LoginScreen.tsx
+++ src/components/organisms/LoginScreen/LoginScreen.tsx
@@ -3,45 +3,85 @@ import Image from "next/image";
 import bgLogin from "../../../../public/img/bg-login.png";
 import TextInput from "../../molecules/FormInput/TextInput";
 import Button from "../../molecules/Button/Button";
+import { sendRequest } from "../../../helpers/apiRequest";
+import Modal from "../../molecules/Modal/Modal";
 
 const LoginScreen: React.FC = () => {
-  const onLogin = () => {
-    localStorage.setItem("isLoggedIn", "true");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isLoading, setLoading] = React.useState(false);
+  const [isError, setError] = React.useState(false);
 
-    // router.push("/dashboard");
-    window.location.href = "/dashboard";
+  const onLogin = async () => {
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    await sendRequest({
+      link: "https://api.payroll-acien.online/C_t_login_user/login",
+      formData,
+    })
+      .then((res) => {
+        setLoading(false);
+        localStorage.setItem("isLoggedIn", "true");
+        localStorage.setItem("userData", JSON.stringify(res.data.data));
+        window.location.href = "/dashboard";
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
   };
+
   return (
-    <div className="w-full h-screen flex animate-fade-in">
-      <div className="hidden h-full relative lg:block lg:w-1/2">
-        <Image src={bgLogin} layout="fill" className="w-full h-full object-cover" />
-      </div>
-      <div className="w-full h-full bg-white flex flex-col items-center justify-between p-8 box-border lg:py-10 lg:px-20 lg:w-1/2 ">
-        <div></div>
-        <div className="w-full flex flex-col items-center">
-          <img src="/img/Logo.svg" className="w-[140px] object-contain" />
-
-          <TextInput
-            label=""
-            placeholder="Username"
-            icon="/img/person-icon.svg"
-            className="mt-[100px] max-w-[500px] "
-          />
-          <TextInput
-            type="password"
-            label=""
-            placeholder="Password"
-            icon="/img/key-icon.svg"
-            className="max-w-[500px] "
-          />
-
-          <Button text="Log in" type="primary" className="mt-[100px]" onClick={onLogin} />
+    <>
+      <Modal title="Notif" width="w-[400px]" show={isError} onShow={() => setError(false)}>
+        <div className="w-full text-center">
+          <h1 className="font-bold text-h6 text-black-800">Username/Password Salah.</h1>
+          <p className="text-body2 mt-4">Harap mencoba kembali.</p>
         </div>
-        <p className="text-caption1 font-normal tracking-widest text-black-400 text-center">
-          {`Made with ❤️ Collaboration From Acien.id & Larsen.dev - 2021`}
-        </p>
+      </Modal>
+      <div className="w-full h-screen flex animate-fade-in">
+        <div className="hidden h-full relative lg:block lg:w-1/2">
+          <Image src={bgLogin} layout="fill" className="w-full h-full object-cover" />
+        </div>
+        <div className="w-full h-full bg-white flex flex-col items-center justify-between p-8 box-border lg:py-10 lg:px-20 lg:w-1/2 ">
+          <div></div>
+          <div className="w-full flex flex-col items-center">
+            <img src="/img/Logo.svg" className="w-[140px] object-contain" />
+
+            <TextInput
+              label=""
+              placeholder="Username"
+              icon="/img/person-icon.svg"
+              className="mt-[100px] max-w-[500px]"
+              onChange={(value) => setUsername(value)}
+            />
+            <TextInput
+              type="password"
+              label=""
+              placeholder="Password"
+              icon="/img/key-icon.svg"
+              className="max-w-[500px] "
+              onChange={(value) => setPassword(value)}
+            />
+
+            <Button
+              text="Log in"
+              type="primary"
+              className="mt-[100px]"
+              onClick={onLogin}
+              isLoading={isLoading}
+            />
+          </div>
+          <p className="text-caption1 font-normal tracking-widest text-black-400 text-center">
+            {`Made with ❤️ Collaboration From Acien.id & Larsen.dev - 2021`}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
