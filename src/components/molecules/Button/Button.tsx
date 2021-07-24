10573069661a801988697c989e35385098fcbbe9
erick <ericklarsenn@gmail.com>
2021-07-25 00:19:05 +0700

    set loader

--- src/components/molecules/Button/Button.tsx
+++ src/components/molecules/Button/Button.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Spinner from "../Spinner/Spinner";
 
 interface Props {
   text: string;
@@ -6,18 +7,28 @@ interface Props {
   stretch?: boolean;
   onClick?: () => void;
   className?: string;
+  isLoading?: boolean;
 }
 
-const Button: React.FC<Props> = ({ text, type, stretch, onClick, className }) => (
+const Button: React.FC<Props> = ({
+  text,
+  isLoading = false,
+  type,
+  stretch,
+  onClick,
+  className,
+}) => (
   <button
-    className={`${stretch ? "w-full" : "px-6"} mt-6 rounded-[10px] font-semibold py-2.5 ${
+    className={`${
+      stretch ? "w-full" : "px-6"
+    } min-w-[120px] mt-6 rounded-[10px] font-semibold py-2.5 ${
       type === "primary"
         ? "bg-yellow-500 hover:bg-yellow-400 text-black-700"
         : type === "secondary" && "bg-black-700 hover:bg-black-600 text-yellow-500"
     } flex items-center justify-center ${className}`}
-    onClick={onClick}
+    onClick={!isLoading ? onClick : () => {}}
   >
-    {text}
+    {!isLoading ? text : <Spinner className="w-6 h-6" />}
   </button>
 );
 
