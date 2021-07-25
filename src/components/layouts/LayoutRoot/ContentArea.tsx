f4e611fdbfecec3c8d0861339700d55a7420b760
erick <ericklarsenn@gmail.com>
2021-07-25 18:30:53 +0700

    Set mobile breakpoint

--- src/components/layouts/LayoutRoot/ContentArea.tsx
+++ src/components/layouts/LayoutRoot/ContentArea.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const ContentArea: React.FC = ({ children }) => {
   return (
     <div
-      className="min-h-screen h-screen overflow-y-auto overflow-x-hidden px-5 lg:px-15 py-12 w-full lg:w-content-field"
+      className="min-h-screen h-screen overflow-y-auto overflow-x-hidden px-5 lg:px-15 pb-[100px] pt-[108px] lg:py-12 w-full lg:w-content-field"
       // style={{ width: "calc(100vw - 276px)" }}
     >
       {children}
